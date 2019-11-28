d8bf262cb46025621ccbc84eac123213f5ed87c5
Javid Shoaei <javidsoft@yahoo.com>
2019-11-28 18:17:35 +0330

    Implement MultiGrabber

---
+++ src/DotNetTools.SharpGrabber/Exceptions/UnsupportedGrabException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetTools.SharpGrabber.Exceptions
+{
+    /// <summary>
+    /// May be thrown when grabbing of a URI is not supported by a grabber or multi-grabber.
+    /// </summary>
+    public class UnsupportedGrabException : GrabException
+    {
+        public UnsupportedGrabException() : this("The specified URI is not supported for grabbing.")
+        {
+        }
+
+        public UnsupportedGrabException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedGrabException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- src/DotNetTools.SharpGrabber/MultiGrabber.cs
+++ src/DotNetTools.SharpGrabber/MultiGrabber.cs
@@ -1,14 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using DotNetTools.NExtension.Structures;
+using DotNetTools.SharpGrabber.Exceptions;
 
 namespace DotNetTools.SharpGrabber
 {
     /// <summary>
     /// Grabs from various sources using grabbers registered on it.
-    /// A multi-grabber cannot grab from several URIs simultaneously because it makes use of.
+    /// Members of this class are thread-safe and can be used simultaneously.
     /// </summary>
+    /// <remarks>
+    /// A multi-grabber can grab from several URIs simultaneously because it creates grabbers using registered factories.
+    /// </remarks>
     public class MultiGrabber
     {
+        #region Fields
+        private readonly HashSet<IFactory<IGrabber>> _grabberFactories = new HashSet<IFactory<IGrabber>>();
+        #endregion
+
+        #region Internal Methods
+        /// <summary>
+        /// Creates a grabber instance using the specified factory.
+        /// May be overriden to modify grabber creation behavior. By default, it uses parameterless constructors.
+        /// </summary>
+        protected virtual IGrabber Make(IFactory<IGrabber> factory) => factory.Create();
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Registers the specified <see cref="IGrabber"/> factory.
+        /// </summary>
+        public void Register(IFactory<IGrabber> factory)
+        {
+            lock (_grabberFactories)
+                _grabberFactories.Add(factory);
+        }
+
+        /// <summary>
+        /// Registers a simple factory for the specified <typeparamref name="TGrabber"/>.
+        /// The registered factory creates instances of the specified type using its parameterless constructor.
+        /// </summary>
+        public void Register<TGrabber>() where TGrabber : IGrabber
+            => Register(new TypeFactory<TGrabber, IGrabber>());
+
+        /// <summary>
+        /// Unregisters the specified factory if previously registered.
+        /// </summary>
+        public void Unregister(IFactory<IGrabber> factory)
+        {
+            lock (_grabberFactories)
+                _grabberFactories.Remove(factory);
+        }
+
+        /// <summary>
+        /// Tries to find all compatible grabbers with the specified URI.
+        /// </summary>
+        public virtual IGrabber[] GetGrabbers(Uri uri)
+        {
+            var result = new List<IGrabber>();
+
+            lock (_grabberFactories)
+            {
+                foreach (var grabberFactory in _grabberFactories)
+                {
+                    var grabber = Make(grabberFactory);
+
+                    if (grabber.Supports(uri))
+                        result.Add(grabber);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Transparently grabs information from the specified URI using the registered grabbers.
+        /// </summary>
+        /// <remarks>
+        /// This method will return the <see cref="GrabResult"/> of the very first successful grab while suppressing exceptions
+        /// thrown by unsuccessful grab attempts. Throws the last exception thrown by the last used grabber.
+        /// <para>Status of the grab is unavailable when using this method because it might automatically make use of multiple
+        /// compatible grabbers. To have status of the grab, use <see cref="GetGrabbers(Uri)"/> and grab manually instead.</para>
+        /// </remarks>
+        public async Task<GrabResult> Grab(Uri uri)
+        {
+            // get all compatible grabbers
+            var grabbers = GetGrabbers(uri);
+
+            // is URI unsupported?
+            if (grabbers.Length == 0)
+                throw new UnsupportedGrabException();
+
+            // grab using the first compatible grabber
+            Exception lastException = null;
+            foreach (var grabber in grabbers)
+            {
+                try
+                {
+                    return await grabber.Grab(uri);
+                }
+                catch (GrabException exception)
+                {
+                    lastException = exception;
+                }
+            }
+
+            // throw the last exception
+            throw lastException;
+        }
+        #endregion
     }
 }
