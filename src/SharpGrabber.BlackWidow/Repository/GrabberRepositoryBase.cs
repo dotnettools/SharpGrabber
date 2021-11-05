149fc71810b2d06a55b0333f6f106b5fec6b9045
Javid <javidsoft@yahoo.com>
2021-11-06 02:05:58 +0330

    Implement change subscription handling in GrabberRepositoryBase

--- src/SharpGrabber.BlackWidow/Repository/GrabberRepositoryBase.cs
+++ src/SharpGrabber.BlackWidow/Repository/GrabberRepositoryBase.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DotNetTools.SharpGrabber.BlackWidow.Internal;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +12,16 @@ namespace DotNetTools.SharpGrabber.BlackWidow.Repository
     /// </summary>
     public abstract class GrabberRepositoryBase : IGrabberRepository
     {
+        private readonly object _monitoringLock = new();
+        private readonly ConcurrentHashSet<IGrabberRepositorySubscription> _subscriptions = new();
         private bool _disposed;
+        private bool _monitoring;
 
         public virtual bool CanPut => false;
 
         public virtual bool CanNotifyChanges => false;
+
+        protected bool AnySubscribers => _subscriptions.Count > 0;
 
         public abstract Task<IGrabberScriptSource> FetchSourceAsync(IGrabberRepositoryScript script);
 
@@ -27,7 +34,13 @@ namespace DotNetTools.SharpGrabber.BlackWidow.Repository
 
         public Task<IGrabberRepositorySubscription> SubscribeAsync()
         {
-            return Task.FromResult<IGrabberRepositorySubscription>(null);
+            IGrabberRepositorySubscription result = null;
+            if (CanNotifyChanges)
+            {
+                result = new Subscription(this);
+                StartOrStopMonitoring();
+            }
+            return Task.FromResult(result);
         }
 
         public void Dispose()
@@ -36,10 +49,80 @@ namespace DotNetTools.SharpGrabber.BlackWidow.Repository
                 return;
             _disposed = true;
             Dispose(true);
+            _subscriptions.Clear();
+            StartOrStopMonitoring();
+        }
+
+        protected void NotifyChanged(IGrabberRepositoryFeed feed)
+        {
+            foreach (var subscription in _subscriptions.OfType<Subscription>())
+                subscription.Notify(feed);
+        }
+
+        /// <summary>
+        /// Starts monitoring for changes to the repository and notifies with .
+        /// </summary>
+        protected virtual Task StartMonitoringAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stops
+        /// </summary>
+        protected virtual Task StopMonitoringAsync()
+        {
+            throw new NotImplementedException();
         }
 
         protected virtual void Dispose(bool disposing)
         {
+        }
+
+        private void Unsubscribe(IGrabberRepositorySubscription subscription)
+        {
+            _subscriptions.Remove(subscription);
+            StartOrStopMonitoring();
+        }
+
+        private void StartOrStopMonitoring()
+        {
+            if (_monitoring == AnySubscribers)
+                return;
+
+            lock (_monitoringLock)
+            {
+                if (_monitoring == AnySubscribers)
+                    return;
+                _monitoring = AnySubscribers;
+
+                if (_monitoring)
+                    _ = StartMonitoringAsync();
+                else
+                    _ = StopMonitoringAsync();
+            }
+        }
+
+        private sealed class Subscription : IGrabberRepositorySubscription
+        {
+            private readonly GrabberRepositoryBase _base;
+
+            public event Action<IGrabberRepositoryFeed, IGrabberRepository> FeedUpdated;
+
+            public Subscription(GrabberRepositoryBase @base)
+            {
+                _base = @base;
+            }
+
+            public void Notify(IGrabberRepositoryFeed feed)
+            {
+                FeedUpdated?.Invoke(feed, _base);
+            }
+
+            public void Dispose()
+            {
+                _base.Unsubscribe(this);
+            }
         }
     }
 }
