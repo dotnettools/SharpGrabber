f098d2a6907f26e903be634ee2fe6d0556fd3025
Javid <javidsoft@yahoo.com>
2021-11-16 04:46:21 +0330

    Add feed difference

--- src/SharpGrabber.BlackWidow/Repository/IGrabberRepositoryFeed.cs
+++ src/SharpGrabber.BlackWidow/Repository/IGrabberRepositoryFeed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,5 +21,38 @@ namespace DotNetTools.SharpGrabber.BlackWidow.Repository
         /// Enumerates descriptors of all scripts in the repository.
         /// </summary>
         IEnumerable<IGrabberRepositoryScript> GetScripts();
+    }
+
+    /// <summary>
+    /// Defines extension methods for <see cref="IGrabberRepositoryFeed"/>.
+    /// </summary>
+    public static class GrabberRepositoryFeedExtensions
+    {
+        /// <summary>
+        /// Gets all the differences between this feed and <paramref name="otherFeed"/>.
+        /// </summary>
+        public static IEnumerable<GrabberRepositoryFeedDifference> GetDifferences(this IGrabberRepositoryFeed feed, IGrabberRepositoryFeed otherFeed)
+        {
+            var dic1 = otherFeed.GetScripts().ToDictionary(s => s.Id);
+            var dic2 = feed.GetScripts().ToDictionary(s => s.Id);
+
+            // return what's new
+            foreach (var id in dic2.Keys.Where(k => !dic1.ContainsKey(k)))
+                yield return new GrabberRepositoryFeedDifference(GrabberRepositoryFeedDifferenceType.ScriptAdded, null, dic2[id]);
+
+            // return what's removed
+            foreach (var id in dic1.Keys.Where(k => !dic2.ContainsKey(k)))
+                yield return new GrabberRepositoryFeedDifference(GrabberRepositoryFeedDifferenceType.ScriptRemoved, dic1[id], null);
+
+            // return what's changed
+            foreach (var id in dic1.Keys.Intersect(dic2.Keys))
+            {
+                var ownScript = dic1[id];
+                var otherScript = dic2[id];
+                if (ownScript.Equals(otherScript))
+                    continue;
+                yield return new GrabberRepositoryFeedDifference(GrabberRepositoryFeedDifferenceType.ScriptChanged, ownScript, otherScript);
+            }
+        }
     }
 }
