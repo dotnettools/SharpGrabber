c68fcaac3f75ac54b46a0a3874fe03c366ddac11
Javid <javidsoft@yahoo.com>
2021-11-19 14:59:55 +0330

    Add MultiGrabberBase

--- src/SharpGrabber/MultiGrabber.cs
+++ src/SharpGrabber/MultiGrabber.cs
@@ -11,39 +11,19 @@ namespace DotNetTools.SharpGrabber
     /// <summary>
     /// Grabs from various sources using grabbers registered on it.
     /// </summary>
-    internal class MultiGrabber : IMultiGrabber
+    internal class MultiGrabber : MultiGrabberBase
     {
-        private readonly ConcurrentHashSet<IGrabber> _grabbers;
-
-        public MultiGrabber(IEnumerable<IGrabber> grabbers, IGrabberServices services)
+        public MultiGrabber(IGrabberServices services) : base(services)
         {
-            Services = services;
-            grabbers ??= Array.Empty<IGrabber>();
-            _grabbers = new ConcurrentHashSet<IGrabber>(grabbers);
-            GrabbersUpdated();
         }
 
-        public MultiGrabber(IGrabberServices services) : this(null, services)
+        public MultiGrabber(IEnumerable<IGrabber> grabbers, IGrabberServices services) : base(grabbers, services)
         {
-
         }
 
-        public string StringId { get; } = null;
-
-        public string Name { get; private set; }
-
-        public GrabOptions DefaultGrabOptions => null;
-
-        public IGrabberServices Services { get; }
-
-        public bool Supports(Uri uri)
-            => _grabbers.Any(g => g.Supports(uri));
-
-        public async Task<GrabResult> GrabAsync(Uri uri, CancellationToken cancellationToken, GrabOptions options, IProgress<double> progress)
+        protected override async Task<GrabResult> InternalGrabAsync(IEnumerable<IGrabber> candidateGrabbers, Uri uri, CancellationToken cancellationToken, GrabOptions options, IProgress<double> progress)
         {
-            progress ??= new Progress<double>();
             Exception lastException = null;
-            var candidateGrabbers = _grabbers.Where(g => g.Supports(uri));
 
             foreach (var grabber in candidateGrabbers)
             {
@@ -63,26 +43,7 @@ namespace DotNetTools.SharpGrabber
                 throw new UnsupportedGrabException("The URL is not supported.");
 
             throw lastException;
-        }
 
-        public IEnumerable<IGrabber> GetRegisteredGrabbers()
-            => _grabbers.AsEnumerable();
-
-        public void Register(IGrabber grabber)
-        {
-            _grabbers.Add(grabber);
-            GrabbersUpdated();
-        }
-
-        public void Unregister(IGrabber grabber)
-        {
-            _grabbers.Remove(grabber);
-            GrabbersUpdated();
-        }
-
-        private void GrabbersUpdated()
-        {
-            Name = string.Join(", ", _grabbers.Select(g => g.Name)).NullIfEmpty();
         }
     }
 }
