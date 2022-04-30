fc8c4f1d7598145ac30d7b18a45a6afef79938af
javid <javidsoft@yahoo.com>
2022-04-30 16:48:02 +0430

    Add tests

---
+++ tests/SharpGrabber.Tests/SharpGrabber.Tests/Adult/PornHubTests.cs
@@ -0,0 +1,24 @@
+﻿using DotNetTools.SharpGrabber;
+using DotNetTools.SharpGrabber.Adult;
+using DotNetTools.SharpGrabber.Grabbed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SharpGrabber.Tests.Adult
+{
+    public class PornHubTests
+    {
+        [Fact]
+        public async Task GrabFromPornHub()
+        {
+            var uri = new Uri("https://www.pornhub.com/view_video.php?viewkey=ph6232156154536");
+            var grabber = new PornHubGrabber(GrabberServices.Default);
+            var result = await grabber.GrabAsync(uri);
+            Assert.Contains(result.Resources, r => r is GrabbedHlsStreamReference || r is GrabbedMedia);
+        }
+    }
+}
---
+++ tests/SharpGrabber.Tests/SharpGrabber.Tests/YouTube/YouTubeTests.cs
@@ -0,0 +1,25 @@
+﻿using DotNetTools.SharpGrabber;
+using DotNetTools.SharpGrabber.Grabbed;
+using DotNetTools.SharpGrabber.YouTube;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SharpGrabber.Tests.YouTube
+{
+    public class YouTubeTests
+    {
+        [Fact]
+        public async Task GrabFromYouTube()
+        {
+            var uri = new Uri("https://www.youtube.com/watch?v=J42SZXS-_Qo");
+            var grabber = new YouTubeGrabber(GrabberServices.Default);
+            var result = await grabber.GrabAsync(uri);
+            Assert.Equal("ASMR Programming - Coding a Snake Game - No Talking", result.Title);
+            Assert.Contains(result.Resources, r => r is GrabbedMedia);
+        }
+    }
+}
