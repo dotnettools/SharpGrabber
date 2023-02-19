76855cd7597d4d777675222799d7f493142da76b
javidsho <javidsoft@yahoo.com>
2023-02-19 20:33:31 +0100

    Add initial implementation of the Odysee grabber

---
+++ src/SharpGrabber.Odysee/OdyseeGrabber.cs
@@ -0,0 +1,38 @@
+﻿using DotNetTools.SharpGrabber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SharpGrabber.Odysee
+{
+    /// <summary>
+    /// Implements <see cref="IGrabber"/> for the Odysee platform.
+    /// </summary>
+    public sealed class OdyseeGrabber : GrabberBase
+    {
+        private static readonly Regex UriRegex = new(
+            @"^https?://(www\.)?odysee\.com/@([\w:]+)/([^\/]+)/?.*$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public OdyseeGrabber(IGrabberServices services) : base(services)
+        {
+        }
+
+        public override string StringId => "odysee.com";
+
+        public override string Name => "Odysee";
+
+        public override bool Supports(Uri uri)
+        {
+            return UriRegex.IsMatch(uri.ToString());
+        }
+
+        protected override Task<GrabResult> InternalGrabAsync(Uri uri, CancellationToken cancellationToken, GrabOptions options, IProgress<double> progress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SharpGrabber.Odysee/OdyseeGrabberBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DotNetTools.SharpGrabber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpGrabber.Odysee
+{
+    /// <summary>
+    /// Defines extension methods on <see cref="IGrabberBuilder"/>.
+    /// </summary>
+    public static class OdyseeGrabberBuilderExtensions
+    {
+        /// <summary>
+        /// Registers the Odysee grabber.
+        /// </summary>
+        public static IGrabberBuilder AddOdysee(this IGrabberBuilder builder)
+            => builder.Add<OdyseeGrabber>();
+    }
+}
